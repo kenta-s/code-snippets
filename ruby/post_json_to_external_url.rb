fc7342d1d006e739965e975d558c9ae6d912980a
kenta-s <knt01222@gmail.com>
2019-04-13 11:57:49 +0900

    post_json_to_external_url

---
+++ ruby/post_json_to_external_url.rb
@@ -0,0 +1,12 @@
+# example:
+# post_json_to_external_url("http://example.com", {text: "hello"}.to_json)
+def post_json_to_external_url(url, payload)
+  uri = URI.parse(url)
+  https = Net::HTTP.new(uri.host, uri.port)
+  https.use_ssl = true
+
+  req = Net::HTTP::Post.new(uri.request_uri)
+  req["Content-Type"] = "application/json"
+  req.body = payload
+  https.request(req)
+end
